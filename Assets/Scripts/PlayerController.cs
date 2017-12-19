848441a929da63bfe975f739378f52ae42bcd3b7
Bakakemono <robinalves1224gmail.com>
2017-12-19 22:32:16 +0100

    Now the player can turn and know when he must stop turn

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,11 +13,20 @@ public class PlayerController : MonoBehaviour
     private float playerMaxSpeed;
     [SerializeField]
     private float playerForceDrag;
+    [SerializeField]
+    private Vector3 ForceTurnRight;
+    [SerializeField]
+    private Vector3 ForceTurnLeft;
+
+
 
     private Rigidbody rigid;
 
     public float speed;
-    
+
+    private bool rightTurn;
+    private bool leftTurn;
+
 
     // Use this for initialization
     void Start()
@@ -28,7 +37,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        speed = rigid.velocity.z;
+        speed = transform.InverseTransformDirection(rigid.velocity).z;
         // Création d'un nouveau vecteur de déplacement
         Vector3 acceleration = new Vector3();
         Vector3 move = new Vector3();
@@ -45,11 +54,60 @@ public class PlayerController : MonoBehaviour
         // Récupération des touches gauche et droite
         move.x += horizontalInput*playerLateralMoveSpeed;
 
+        
+
         // On applique le mouvement à l'objet
-        if (rigid.velocity.z <= playerMaxSpeed && rigid.velocity.z >= 0)
+        if (transform.InverseTransformDirection(rigid.velocity).z <= playerMaxSpeed && transform.InverseTransformDirection(rigid.velocity).z >= 0)
         {
-            rigid.AddForce(acceleration, ForceMode.Acceleration);
+          
+            if (transform.InverseTransformDirection(rigid.velocity).z > 0 && rightTurn)
+            {
+                transform.Rotate(ForceTurnRight * transform.InverseTransformDirection(rigid.velocity).z);
+                rigid.velocity = transform.forward * speed;
+                rigid.AddRelativeForce(acceleration, ForceMode.Acceleration);
+                rigid.AddRelativeForce(move, ForceMode.VelocityChange);
+
+            }
+            else if (transform.InverseTransformDirection(rigid.velocity).z > 0 && leftTurn)
+            {
+                transform.Rotate(ForceTurnLeft * transform.InverseTransformDirection(rigid.velocity).z);
+                rigid.velocity = transform.forward;
+                rigid.AddRelativeForce(acceleration, ForceMode.Acceleration);
+                rigid.AddRelativeForce(move, ForceMode.VelocityChange);
+
+            }
+            else
+            {
+                rigid.AddRelativeForce(acceleration, ForceMode.Acceleration);
+                rigid.AddRelativeForce(move, ForceMode.VelocityChange);
+            }
         }
-        rigid.AddForce(move, ForceMode.VelocityChange);
+        
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "BeginRightTurn")
+        {
+            
+            rightTurn = true;
+        }
+
+        if (other.tag == "BeginLeftTurn")
+        {
+            leftTurn = true;
+        }
+
+        if (other.tag == "EndRightTurn")
+        {
+            
+            rightTurn = false;
+        }
+
+        if (other.tag == "EndLeftTurn")
+        {
+            leftTurn = false;
+        }
     }
 }
